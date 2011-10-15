95380ba5f4b6a7c1a9f6c5039f5f78d3cc1841b5
Rob Caelers <robc@krandor.org>
2011-10-15 13:13:22 +0200

    Add exmaple thar controls workrave.
    Signed-off-by: Rob Caelers <robc@krandor.org>

---
+++ examples/Workrave.cs
@@ -0,0 +1,61 @@
+﻿using DBus;
+
+namespace org.workrave
+{
+    public delegate void BreakChangedEventHandler(string progress);
+
+    [Interface("org.workrave.CoreInterface")]
+    public interface ICore
+    {
+        void SetOperationMode(string mode);
+        string GetOperationMode();
+
+        void ReportActivity(string who, bool act);
+        bool IsTimerRunning(string timer_id);
+        int GetTimerIdle(string timer_id);
+        int GetTimerElapsed(string timer_id);
+        int GetTimerOverdue(string timer_id);
+
+        int GetTime();
+        bool IsActive();
+
+        void PostponeBreak(string timer_id);
+        void SkipBreak(string timer_id);
+
+        event BreakChangedEventHandler MicrobreakChanged;
+        event BreakChangedEventHandler RestbreakChanged;
+        event BreakChangedEventHandler DailylimitChanged;
+    }
+
+    [Interface("org.workrave.ConfigInterface")]
+    public interface IConfig
+    {
+        bool SetString(string key, string value);
+        bool SetInt(string key, int value);
+        bool SetBool(string key, bool value);
+        bool SetDouble(string key, double value);
+
+        bool GetString(string key, out string value);
+        bool GetInt(string key, out int value);
+        bool GetBool(string key, out bool value);
+        bool GetDouble(string key, out double value);
+    }
+
+    [Interface("org.workrave.ControlInterface")]
+    public interface IControl
+    {
+        void OpenMain();
+        void Preferences();
+        void NetworkConnect();
+        void NetworkLog(bool show);
+        void NetworkReconnect();
+        void NetworkDisconnect();
+        void ReadingMode(bool on);
+        void Statistics();
+        void Exercises();
+        void RestBreak();
+        void Quit();
+        void About();
+    }
+}
+
---
+++ examples/WorkraveExample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using DBus;
+using org.freedesktop.DBus;
+using org.workrave;
+
+class WorkraveExample
+{
+    static Connection bus = null;
+
+    public static int Main(string[] args)
+    {
+        bus = Bus.Session;
+
+        ObjectPath corepath = new ObjectPath("/org/workrave/Workrave/Core");
+        ObjectPath uipath = new ObjectPath("/org/workrave/Workrave/UI");
+        string name = "org.workrave.Workrave";
+
+        ICore core = bus.GetObject<ICore>(name, corepath);
+        IConfig config = bus.GetObject<IConfig>(name, corepath);
+        IControl control = bus.GetObject<IControl>(name, uipath);
+
+        //control.ReadingMode(true);
+        //control.RestBreak();
+        //control.Statistics();
+
+        //int v;
+        //bool b = config.GetInt("timers/micro_pause/limit", out v);
+        //config.SetInt("timers/micro_pause/limit", 201);
+        //b = config.GetInt("timers/micro_pause/limit", out v);
+
+        //string opmode = core.GetOperationMode();
+        //core.SetOperationMode("normal");
+        //opmode = core.GetOperationMode();
+        //b = core.IsActive();
+        //b = core.IsTimerRunning("microbreak");
+        //int t = core.GetTime();
+        //t = core.GetTimerElapsed("restbreak");
+        //t = core.GetTimerIdle("restbreak");
+        //t = core.GetTimerOverdue("restbreak");
+        //core.PostponeBreak("microbreak");
+        //core.SkipBreak("microbreak");
+
+        core.MicrobreakChanged += new BreakChangedEventHandler(OnMicrobreakChanged);
+
+        while (true)
+        {
+            bus.Iterate();
+        }
+
+        return 0;
+    }
+
+    static void OnMicrobreakChanged(string progress)
+    {
+        Console.WriteLine("Micro Break " + progress);
+    }
+}
